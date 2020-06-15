67c9aafaeed78d6c2d37ed935224914650285256
Mike Fehrenbach <fehrenbach.mike@gmail.com>
2020-06-14 22:15:45 -0400

    Update Exercise1.vue to a more relevant name

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="app">
     <OneWayBinding />
-    <Exercise1 />
+    <OneWayBindingExercise />
     <Events />
   </div>
 </template>
 
 <script>
 import OneWayBinding from './components/OneWayBinding.vue'
-import Exercise1 from './components/Exercise1.vue'
+import OneWayBindingExercise from './components/OneWayBindingExercise.vue'
 import Events from './components/Events.vue'
 
 export default {
   name: 'App',
   components: {
     OneWayBinding,
-    Exercise1,
+    OneWayBindingExercise,
     Events
   }
 }
