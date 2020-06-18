8cac44dadf4893e99eb129f165835c09b375de51
Mike Fehrenbach <fehrenbach.mike@gmail.com>
2020-06-18 18:18:30 -0400

    computed function example
    computed functions are only called when the dependent data values are updated.
    Methods are updated every time the DOM is updated

--- src/components/TwoWayBinding.vue
+++ src/components/TwoWayBinding.vue
@@ -3,6 +3,11 @@
     <h1 v-once>{{ title }}</h1>
     <input type='text' v-model='name'/>
     <p>{{ name }}</p>
+    <button v-on:click='counter++'>Increase</button>
+    <button v-on:click='counter--'>Decrease</button>
+    <button v-on:click='secondCounter++'>Increase Second</button>
+    <p>Counter: {{ counter }} | {{ secondCounter }}</p>
+    <p>Result: {{ result() }} | {{ output }}</p>
   </div>
 </template>
 
@@ -12,8 +17,23 @@ export default {
   data() {
 		return {
       title: 'TwoWayBinding',
-      name: 'Mike'
-    };
+      name: 'Mike',
+      counter: 0,
+      secondCounter: 0,
+    }
+  },
+  computed: {
+    output() {
+      console.log('Computed')
+      return this.counter > 5 ? 'Greater than 5' : 'Less than 5';
+    
+    }
+  },
+  methods: {
+    result() {
+      console.log('Method')
+      return this.counter > 5 ? 'Greater than 5' : 'Less than 5';
+    }
   }
 }
 </script>
