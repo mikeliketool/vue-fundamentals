354474e0891216793d8507407b1bcb11e89fb4c8
Mike Fehrenbach <fehrenbach.mike@gmail.com>
2021-02-13 22:44:51 -0500

    Move component inclusion to main.js

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,21 @@
 <template>
   <div id="app">
-    <OneWayBinding />
-    <OneWayBindingExercise />
-    <Events />
-    <EventsExercise />
-    <TwoWayBinding />
-    <TwoWayBindingExercise />
-    <DynamicCSS />
-    <DynamicCSSExercise />
-    <ConditionalRendering />
-    <ListRendering />
-    <ConditionalRenderingAndListRenderingExercise />
+    <one-way-binding></one-way-binding>
+    <one-way-binding-exercise></one-way-binding-exercise>
+    <events></events>
+    <events-exercise></events-exercise>
+    <two-way-binding></two-way-binding>
+    <two-way-binding-exercise></two-way-binding-exercise>
+    <dynamic-css></dynamic-css>
+    <dynamic-css-exercise></dynamic-css-exercise>
+    <conditonal-rendering></conditonal-rendering>
+    <list-rendering></list-rendering>
+    <conditional-and-list-rendering-exercise></conditional-and-list-rendering-exercise>
   </div>
 </template>
 
 <script>
-import OneWayBinding from './components/OneWayBinding.vue'
-import OneWayBindingExercise from './components/OneWayBindingExercise.vue'
-import Events from './components/Events.vue'
-import EventsExercise from './components/EventsExercise.vue'
-import TwoWayBinding from './components/TwoWayBinding.vue'
-import TwoWayBindingExercise from './components/TwoWayBindingExercise.vue'
-import DynamicCSS from './components/DynamicCSS.vue'
-import DynamicCSSExercise from './components/DynamicCSSExercise.vue'
-import ConditionalRendering from './components/ConditionalRendering.vue'
-import ListRendering from './components/ListRendering.vue'
-import ConditionalRenderingAndListRenderingExercise from './components/ConditionalRenderingAndListRenderingExercise.vue'
-
 export default {
-  name: 'App',
-  components: {
-    OneWayBinding,
-    OneWayBindingExercise,
-    Events,
-    EventsExercise,
-    TwoWayBinding,
-    TwoWayBindingExercise,
-    DynamicCSS,
-    DynamicCSSExercise,
-    ConditionalRendering,
-    ListRendering,
-    ConditionalRenderingAndListRenderingExercise
-  }
+  name: 'App'
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
 import  {createApp}   from 'vue'
 import App from './App.vue'
+import OneWayBinding from './components/OneWayBinding.vue'
+import OneWayBindingExercise from './components/OneWayBindingExercise.vue'
+import Events from './components/Events.vue'
+import EventsExercise from './components/EventsExercise.vue'
+import TwoWayBinding from './components/TwoWayBinding.vue'
+import TwoWayBindingExercise from './components/TwoWayBindingExercise.vue'
+import DynamicCSS from './components/DynamicCSS.vue'
+import DynamicCSSExercise from './components/DynamicCSSExercise.vue'
+import ConditionalRendering from './components/ConditionalRendering.vue'
+import ListRendering from './components/ListRendering.vue'
+import ConditionalRenderingAndListRenderingExercise from './components/ConditionalRenderingAndListRenderingExercise.vue'
 
 
 const app = createApp(App);
+app.component('one-way-binding', OneWayBinding)
+app.component('one-way-binding-exercise', OneWayBindingExercise)
+app.component('events', Events)
+app.component('events-exercise', EventsExercise)
+app.component('two-way-binding', TwoWayBinding)
+app.component('two-way-binding-exercise', TwoWayBindingExercise)
+app.component('dynamic-css', DynamicCSS)
+app.component('dynamic-css-exercise', DynamicCSSExercise)
+app.component('conditonal-rendering', ConditionalRendering)
+app.component('list-rendering', ListRendering)
+app.component('conditional-and-list-rendering-exercise', ConditionalRenderingAndListRenderingExercise)
 app.mount('#app');
